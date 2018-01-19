51562f9ba58555aa78726124df726a45b47e6f1c
rkepha <kephaokari@gmail.com>
2018-01-19 15:05:30 +0300

    added the views to the home page with httpresponse

--- gallery/pixapp/views.py
+++ gallery/pixapp/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
 
 # Create your views here.
+def welcome(request):
+    return HttpResponse('Welcome to PixWall')
+
+
+
+def show_date(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+
+    html = f'''
+        <html>
+            <body>
+                <h1>News for KEPHA</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
