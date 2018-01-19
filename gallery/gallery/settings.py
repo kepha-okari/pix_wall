4bb5d15f818ff59e1872a6c24d0e5a2127aa10ac
rkepha <kephaokari@gmail.com>
2018-01-19 16:17:14 +0300

    added the home page

--- gallery/gallery/settings.py
+++ gallery/gallery/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'pixapp.apps.PixappConfig',
+    'pixapp.apps.PixappConfig',
 ]
 
 MIDDLEWARE = [
