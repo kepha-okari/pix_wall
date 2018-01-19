8ea066000a03f4ddad0f984186b65d992767ef6b
rkepha <kephaokari@gmail.com>
2018-01-19 15:56:28 +0300

    added a primitive template routes

--- gallery/pixapp/views.py
+++ gallery/pixapp/views.py
@@ -2,22 +2,32 @@ from django.shortcuts import render
 from django.http  import HttpResponse
 import datetime as dt
 
-# Create your views here.
+# # Create your views here.
+# def welcome(request):
+#     return HttpResponse('Welcome to PixWall')
+
 def welcome(request):
-    return HttpResponse('Welcome to PixWall')
-
-
+    return render(request, 'welcome.html')
 
 def show_date(request):
     date = dt.date.today()
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
-
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
-                <h1>News for KEPHA</h1>
+                <h1>{day} {date.day}-{date.month}-{date.year}</h1>
+                <p
             </body>
         </html>
             '''
     return HttpResponse(html)
+def convert_dates(dates):
+    #funticon that gets the weekday number for the dates
+    day_number = dt.date.weekday(dates)
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+    #rReturning the actual date of the weekday
+    day = days[day_number]
+    return day
